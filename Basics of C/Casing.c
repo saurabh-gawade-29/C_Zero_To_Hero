2b0d6f0852666acd94778d57bd0f264c4bbffa74
Saurabh Gawade <saurabhgawade2920@gmail.com>
2026-06-30 15:26:21 +0530

    Update Casing

---
+++ Basics of C/Casing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    // camel case - full name
+    char fullName = "J";
+    char myGrade = 'A';
+    // pascal case
+    char MyGrade = 'B';
+    // snake case
+    char my_grade = 'C';
+    printf("%c %c %c", myGrade, MyGrade, my_grade);
+    return 0;
+
+    int shlokInarkar;  // camel case
+    int ShlokInarkar;  // pascal case
+    int shlok_inarkar; // snake case
+}
