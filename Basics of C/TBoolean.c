c6076157e6f05d0f19359578502afb4bec16ef4a
Saurabh Gawade <saurabhgawade2920@gmail.com>
2026-07-15 22:01:00 +0530

    update

---
+++ Basics of C/TBoolean.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool a = true; // do not use double quotes for boolean values, use true or false instead
+    bool b = false;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    return 0;
+}
